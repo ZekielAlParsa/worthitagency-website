73bd61ad5a00234d5dc13139760b9352529b13f4
Yannis Sulfourt <yannis.sulfourt@gmail.com>
2018-02-08 16:22:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+﻿var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
